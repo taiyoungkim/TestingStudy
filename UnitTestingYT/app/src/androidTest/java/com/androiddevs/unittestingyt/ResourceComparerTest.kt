5e444c4c64a3a74b88cb8ebb9a0554dcd1f23aa5
taiyoungkim <ktyoung153@gmail.com>
2022-08-26 15:00:08 +0900

    TDD Study day 2

---
+++ UnitTestingYT/app/src/androidTest/java/com/androiddevs/unittestingyt/ResourceComparerTest.kt
@@ -0,0 +1,37 @@
+package com.androiddevs.unittestingyt
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest{
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @After
+    fun teardown() {
+        
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_resultsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "UnitTestingYT")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_resultsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Hello")
+        assertThat(result).isFalse()
+    }
+}
